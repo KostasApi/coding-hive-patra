beecda141975a26794947be3e2c31cd11a782083
Konstantinos Apidopoulos <konstantinos.apidopoulos@intrasoft-intl.com>
2019-12-12 00:09:53 +0200

    integrate redux

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,41 +1,35 @@
 import React from 'react';
 import { Card, Rate } from 'antd';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import service from '../services/BusinessService';
+import { fetchBusinessDetails } from '../actions/businessActions';
 
 const { Meta } = Card;
 
-const Business = ({ details, setLoading, setError }) => {
-  let history = useHistory();
-
-  const getBusinessDetails = async () => {
-    setLoading();
-    const data = await service.getBusinessDetails(details.id);
-    setLoading();
-    if (data.isAxiosError) {
-      setError(data.message);
-    } else {
-      history.push(`business-details/${details.id}`, data);
-    }
-  };
-
+const Business = ({ details, getBusinessDetails }) => {
   return (
-    <Card
-      hoverable
-      style={{ width: '100%' }}
-      cover={
-        <img style={{ height: 400 }} alt="example" src={details.image_url} />
-      }
-      onClick={getBusinessDetails}
-    >
-      <Meta title={details.name} style={{ marginBottom: 5 }} />
-      <div>{`Address: ${details.location.address1}`}</div>
-      <div>{`Phone: ${details.display_phone}`}</div>
-      <Rate allowHalf value={details.rating} />
-      <div>{`Reviews: ${details.review_count}`}</div>
-    </Card>
+    <Link to={`/business-details/${details.id}`}>
+      <Card
+        hoverable
+        style={{ width: '100%' }}
+        cover={
+          <img style={{ height: 400 }} alt="example" src={details.image_url} />
+        }
+        onClick={() => getBusinessDetails(details.id)}
+      >
+        <Meta title={details.name} style={{ marginBottom: 5 }} />
+        <div>{`Address: ${details.location.address1}`}</div>
+        <div>{`Phone: ${details.display_phone}`}</div>
+        <Rate allowHalf value={details.rating} />
+        <div>{`Reviews: ${details.review_count}`}</div>
+      </Card>
+    </Link>
   );
 };
 
-export default Business;
+const mapDispatchToProps = dispatch => ({
+  getBusinessDetails: id => dispatch(fetchBusinessDetails(id))
+});
+
+export default connect(null, mapDispatchToProps)(Business);
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import business from './businessReducer';
+
+export default combineReducers({
+  business
+});
