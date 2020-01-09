2d5c17b568130ab7e222aff706b358e5dd9adc95
Konstantinos Apidopoulos <konstantinos.apidopoulos@intrasoft-intl.com>
2020-01-09 11:32:43 +0200

    Updates

---
+++ precache-manifest.159ce866a0105a435478d7cb0198fb19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235aa6d674916a2332d027f3344da7fd",
+    "url": "/coding-hive-patra/index.html"
+  },
+  {
+    "revision": "044768e6c4889065e558",
+    "url": "/coding-hive-patra/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "f7a740fbb3bc4d912173",
+    "url": "/coding-hive-patra/static/css/main.b71be294.chunk.css"
+  },
+  {
+    "revision": "044768e6c4889065e558",
+    "url": "/coding-hive-patra/static/js/2.c4084ba6.chunk.js"
+  },
+  {
+    "revision": "f7a740fbb3bc4d912173",
+    "url": "/coding-hive-patra/static/js/main.53d3a4d9.chunk.js"
+  },
+  {
+    "revision": "a83adf5301514c0ad303",
+    "url": "/coding-hive-patra/static/js/runtime-main.2d077865.js"
+  },
+  {
+    "revision": "649a6d47f28091883e77fe179c162fd9",
+    "url": "/coding-hive-patra/static/media/loading.649a6d47.gif"
+  }
+]);
